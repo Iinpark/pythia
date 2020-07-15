7b5eee41851e20637386f13d5fbadb52d2b976e4
Sultan Esbolatov <linpark11197@gmail.com>
2020-07-15 19:46:04 +0600

    Initial Commit

---
+++ components/LaunchCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+// challenge: only hooks
+const LaunchCard = () => {
+  return (
+    <View>
+      <Text>Привет</Text>
+    </View>
+  );
+};
+
+export default LaunchCard
